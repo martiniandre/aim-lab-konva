ef59369775ca1f972af2c481bf638bfda0a3286c
unknown <andremartini.10@gmail.com>
2021-09-28 22:29:08 -0300

    change stage width and height

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
     setInterval(() => {
       const newBall = {
         id: Math.floor(Math.random() * 100000),
-        x: Math.floor(Math.random() * 1000),
+        x: Math.floor(Math.random() * 1100),
         y: Math.floor(Math.random() * 400),
         height: 30,
         width: 30,
